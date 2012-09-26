eb2afc3352f660b11d001c6fec5460753642a8d9
Abhi <abhi.nemani@gmail.com>
2012-09-26 11:41:57 -0700

    Swapped from separate page for app to modal

--- app.js
+++ app.js
@@ -1,35 +1,4 @@
-$(document).ready(function() {
 
-	var show_application = function(){
-		$("#information").hide();
-		$("#application").show();
-		window.scrollTo(0,500);
-	}
-
-	$("#application").hide(); 
-
-	$(".Apply .button").live( 'click', function() {
-		show_application();
-		window.location.hash = '#application';
-		
-		
-		//Firefox for some reason does not refresh the DOM in iFrame
-	    if($.browser.mozilla){
-			document.getElementsByTagName('iframe')[0].src=document.getElementsByTagName('iframe')[0].src
-	    }
-
-	});
-
-
-
-
-
-	$("div#application a.back").click(function() {
-		$("#application").hide();
-		$("#information").show();
-		window.location.hash = '';
-	});
-		
 	$('.advisorimages ul li img').popover({
 		placement: 'bottom',
 		animation: false}
